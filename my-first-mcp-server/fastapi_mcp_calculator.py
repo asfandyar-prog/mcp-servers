2204cb47b9bc38ca00b43a4476f74d4ed827a04b
Asfand Yar <asfandyar@mailbox.unideb.hu>
2026-03-19 05:14:37 +0100

    feat: add FastAPI + MCP calculator server with Swagger UI

---
+++ my-first-mcp-server/fastapi_mcp_calculator.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Calculator API")
+
+@app.post("/multiply")
+def multiply(a:float,b:float):
+    """
+    Multiply two numbers and returns the result
+    
+    """
+
+    result = a * b
+
+    return {"result": result}
+
+
+@app.post("/add")
+def add(a:float,b:float):
+    """
+    Add two numbers and returns the result
+    
+    """
+
+    result = a + b
+
+    return {"result ": result}
+
+
+@app.post("/subtraction")
+def subtraction(a:float,b:float):
+    """
+    Subtract two numbers and returns the result
+    
+    """
+
+    result = a - b
+
+    return {"result ": result}
+
+@app.post("/division")
+def division(a:float,b:float):
+    """
+    Divide two numbers and returns the result
+    
+    """
+
+    result = a / b
+
+    return {"result ": result}
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    print("Starting the Calculator API server...")
+    uvicorn.run(app, host="localhost", port=8002)
