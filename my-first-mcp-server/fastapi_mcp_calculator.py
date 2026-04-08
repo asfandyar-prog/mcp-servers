9fd03be4fe4e4ad66af4b635f93a0747726fb7e1
Asfand Yar <asfandyar@mailbox.unideb.hu>
2026-04-08 21:43:05 +0200

    Add arithmetic endpoints for multiply, add, subtract, and divide
    Implement basic arithmetic operations in FastAPI.

--- my-first-mcp-server/fastapi_mcp_calculator.py
+++ my-first-mcp-server/fastapi_mcp_calculator.py
@@ -1,1 +1,65 @@
 
+from fastapi import FastAPI
+from fastapi_mcp import FastApiMCP
+
+app = FastAPI(title="Calculator API")
+
+@app.post("/multiply")
+def multiply(a:float,b:float):
+    """
+    Multiply two numbers and returns the result
+    
+    """
+
+    result = a * b
+
+    return {"result": result}
+
+
+@app.post("/add")
+def add(a:float,b:float):
+    """
+    Add two numbers and returns the result
+    
+    """
+
+    result = a + b
+
+    return {"result ": result}
+
+
+@app.post("/subtraction")
+def subtraction(a:float,b:float):
+    """
+    Subtract two numbers and returns the result
+    
+    """
+
+    result = a - b
+
+    return {"result ": result}
+
+@app.post("/division")
+def division(a:float,b:float):
+    """
+    Divide two numbers and returns the result
+    
+    """
+    if b==0:
+        return {"Error":"Divsion by zero is not allowed"}
+
+    result = a / b
+
+    return {"result ": result}
+
+
+
+# Converting into mcp
+mcp=FastApiMCP(app,name="Calculator mcp")
+mcp.mount()
+
+if __name__ == "__main__":
+    import uvicorn
+    print("Starting the Calculator API server...")
+    uvicorn.run(app, host="localhost", port=8002)
+
